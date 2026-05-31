693cd52317813bdc548144a9eec16e5589350863
Gabriel <gmoreiraramos63@gmail.com>
2026-05-31 12:39:47 -0300

    pwa vortex

---
+++ public/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "vortex-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/login.html",
+  "/login.css",
+  "/login.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
